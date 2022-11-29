aa27ed72d65f8b2be90f42d62976286d2cd4c573
Ardo <90957355+axdevelopment@users.noreply.github.com>
2022-11-29 10:34:08 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,10 @@
+-- CONFIG ITEM NAMES
+local BulletproofItem		= 'bulletproof'
+local MedikitItem		= 'medikit'
+local BandageItem		= 'bandage'
+local ClipItem			= 'clip'
+local BoxClipItem		= 'boxclip'
+
 AddEventHandler('onResourceStart', function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
       return
@@ -6,40 +13,40 @@ AddEventHandler('onResourceStart', function(resourceName)
 end)
 
 -- BULLETPROOF
-ESX.RegisterUsableItem(AX.BulletproofItem, function(source)
+ESX.RegisterUsableItem(BulletproofItem, function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
 	TriggerClientEvent('ax_items:bulletproof', source)
-	xPlayer.removeInventoryItem(AX.BulletproofItem, 1)
+	xPlayer.removeInventoryItem(BulletproofItem, 1)
 end)
 
 -- MEDIKIT
-ESX.RegisterUsableItem(AX.MedikitItem, function(source)
+ESX.RegisterUsableItem(MedikitItem, function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
     TriggerClientEvent('ax_items:medikit', source)
-    xPlayer.removeInventoryItem(AX.MedikitItem, 1)
+    xPlayer.removeInventoryItem(MedikitItem, 1)
 end)
 
 -- BANDAGE
-ESX.RegisterUsableItem(AX.BandageItem, function(source)
+ESX.RegisterUsableItem(BandageItem, function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
     TriggerClientEvent('ax_items:bandage', source)
-    xPlayer.removeInventoryItem(AX.BandageItem, 1)
+    xPlayer.removeInventoryItem(BandageItem, 1)
 end)
 
 -- AMMO CLIP
-ESX.RegisterUsableItem(AX.ClipItem, function(source)
+ESX.RegisterUsableItem(ClipItem, function(source)
 	TriggerClientEvent('ax_items:clip', source)
 end)
 RegisterNetEvent('ax_items:remove:clip', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem(AX.ClipItem, 1)
+	xPlayer.removeInventoryItem(ClipItem, 1)
 end)
 
 -- AMMO BOX
-ESX.RegisterUsableItem(AX.BoxClipItem, function(source)
+ESX.RegisterUsableItem(BoxClipItem, function(source)
 	TriggerClientEvent('ax_items:boxclip', source)
 end)
 RegisterNetEvent('ax_items:remove:boxclip', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem(AX.BoxClipItem, 1)
+	xPlayer.removeInventoryItem(BoxClipItem, 1)
 end)
