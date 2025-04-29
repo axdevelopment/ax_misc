c9c4b63b46ebbb049565d9e3e3b6fcb7e00d1e64
Ardo <90957355+axdevelopment@users.noreply.github.com>
2025-04-29 20:57:45 +0200

    ops

--- server/server.lua
+++ server/server.lua
@@ -1,57 +1,57 @@
 -- CONFIG ITEM NAMES
-local BulletproofItem		= 'bulletproof'
-local MedikitItem		= 'medikit'
-local BandageItem		= 'bandage'
-local ClipItem			= 'clip'
-local BoxClipItem		= 'boxclip'
+--local BulletproofItem		= 'bulletproof'
+--local MedikitItem		= 'medikit'
+--local BandageItem		= 'bandage'
+--local ClipItem			= 'clip'
+--local BoxClipItem		= 'boxclip'
 
-AddEventHandler('onResourceStart', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-      return
-    end
-    print('Resource successfully started!')
-end)
+--AddEventHandler('onResourceStart', function(resourceName)
+--    if (GetCurrentResourceName() ~= resourceName) then
+--      return
+--    end
+--    print('Resource successfully started!')
+--end)
 
 -- BULLETPROOF
-ESX.RegisterUsableItem(BulletproofItem, function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-	TriggerClientEvent('ax_items:bulletproof', source)
-	xPlayer.removeInventoryItem(BulletproofItem, 1)
-end)
+--ESX.RegisterUsableItem(BulletproofItem, function(source)
+--    local xPlayer = ESX.GetPlayerFromId(source)
+--	TriggerClientEvent('ax_items:bulletproof', source)
+--	xPlayer.removeInventoryItem(BulletproofItem, 1)
+--end)
 
 -- MEDIKIT
-ESX.RegisterUsableItem(MedikitItem, function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerClientEvent('ax_items:medikit', source)
-    xPlayer.removeInventoryItem(MedikitItem, 1)
-end)
+--ESX.RegisterUsableItem(MedikitItem, function(source)
+--    local xPlayer = ESX.GetPlayerFromId(source)
+--    TriggerClientEvent('ax_items:medikit', source)
+--    xPlayer.removeInventoryItem(MedikitItem, 1)
+--end)
 
 -- BANDAGE
-ESX.RegisterUsableItem(BandageItem, function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerClientEvent('ax_items:bandage', source)
-    xPlayer.removeInventoryItem(BandageItem, 1)
-end)
+--ESX.RegisterUsableItem(BandageItem, function(source)
+--    local xPlayer = ESX.GetPlayerFromId(source)
+--    TriggerClientEvent('ax_items:bandage', source)
+--    xPlayer.removeInventoryItem(BandageItem, 1)
+--end)
 
 -- AMMO CLIP
-ESX.RegisterUsableItem(ClipItem, function(source)
-	TriggerClientEvent('ax_items:clip', source)
-end)
-RegisterNetEvent('ax_items:remove:clip', function()
-	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem(ClipItem, 1)
-end)
+--ESX.RegisterUsableItem(ClipItem, function(source)
+--	TriggerClientEvent('ax_items:clip', source)
+--end)
+--RegisterNetEvent('ax_items:remove:clip', function()
+--	local xPlayer = ESX.GetPlayerFromId(source)
+--	xPlayer.removeInventoryItem(ClipItem, 1)
+--end)
 
 -- AMMO BOX
-ESX.RegisterUsableItem(BoxClipItem, function(source)
-	TriggerClientEvent('ax_items:boxclip', source)
-end)
-RegisterNetEvent('ax_items:remove:boxclip', function()
-	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem(BoxClipItem, 1)
-end)
+--ESX.RegisterUsableItem(BoxClipItem, function(source)
+--	TriggerClientEvent('ax_items:boxclip', source)
+--end)
+--RegisterNetEvent('ax_items:remove:boxclip', function()
+--	local xPlayer = ESX.GetPlayerFromId(source)
+--	xPlayer.removeInventoryItem(BoxClipItem, 1)
+--end)
 
--- [[ UNTESTED OPTIMIZATION, TO TEST THIS, JUST DELETE THE CODE ABOVE AND UNCOMMENT THE CODE BELOW
+-- UNTESTED OPTIMIZATION
 
 -- CONFIG ITEM NAMES
 local items = {
@@ -85,5 +85,3 @@ for itemName, config in pairs(items) do
         end)
     end
 end
-
-]]
